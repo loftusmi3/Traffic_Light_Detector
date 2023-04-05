3c5078aa20eb6e615fa9471d7aa06280b8a3353b
MuayTy <55101408+MuayTy@users.noreply.github.com>
2023-04-04 21:07:20 -0400

    Create color_lights.py

---
+++ color_lights.py
@@ -0,0 +1,9 @@
+import nump as np
+
+"""
+given an image and location of traffic signal,
+check what color light is currently active.
+"""
+def check_color(img, coords):
+  color = None
+  return color
