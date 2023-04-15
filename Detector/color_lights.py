d0915ecb530618dfcdece2aae6bf0e881d534239
tburdick93 <tburdick@controlgaging.com>
2023-04-15 13:18:28 -0400

    takes in numpy array instead of image. fixed yellow mask

--- Detector/color_lights.py
+++ Detector/color_lights.py
@@ -6,45 +6,33 @@ import argparse
 import pdb
 
 """
-given an image and location of traffic signal,
-check what color light is currently active.
+given a numpy array that contains image data of a traffic light,
+detects the color that the traffic light is and returns a BGR tuple
+for either red, yellow, or green.
 """
-def get_color(img):
-
-    # # load the image
-    # imageFrame = Image.open('/Valid_Traffic_Lights/traffic_light0_00001.jpg')
-
-    # convert image to numpy array
-    # data = np.array(imageFrame)
-    data = np.array(img)
-
-    # # git the detected slice of image
-    # x1, y1, x2, y2 = coords
-    # data = data[y1:y2,x1:x2]
+def get_color(data):
 
     # convert to HSV
     hsvFrame = cv2.cvtColor(data, cv2.COLOR_BGR2HSV)
-    #plt.imsave("test-hsv.jpg", img)
 
     # Set range for red color and
     # define mask
     # red_lower = np.array([136, 87, 111], np.uint8)
     # red_upper = np.array([180, 255, 255], np.uint8)
-    red_lower = np.array([100, 87, 111], np.uint8)
+    red_lower = np.array([116, 28, 111], np.uint8)
     red_upper = np.array([180, 255, 255], np.uint8)
-    #pdb.set_trace()
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
 
     # Set range for yellow color and
     # define mask
-    yellow_lower = np.array([20, 100, 100], np.uint8)
-    yellow_upper = np.array([30, 255, 255], np.uint8)
+    yellow_lower = np.array([105, 115, 100], np.uint8)
+    yellow_upper = np.array([115, 200, 160], np.uint8)
     yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper)
 
     # Set range for green color and
     # define mask
-    green_lower = np.array([25, 52, 72], np.uint8)
-    green_upper = np.array([102, 255, 255], np.uint8)
+    green_lower = np.array([25, 80, 40], np.uint8)
+    green_upper = np.array([50, 255, 255], np.uint8)
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
 
     # determine percentage that the light is red, yellow, or green.
@@ -62,6 +50,7 @@ def get_color(img):
     c = np.argmax([red_percent, yellow_percent, green_percent])
 
     #print(f'detected color is {color}')
+
     # BGR color for OpenCV
     if c == 0:
         return (0,0,255)
@@ -82,5 +71,19 @@ if __name__ == "__main__":
     while len(args.number) < 5:
         args.number = '0' + args.number
     imageFrame = Image.open(('./Valid_Traffic_Lights/traffic_light0_%s.jpg' % args.number))
-    color = check_color(imageFrame)
+
+    # # red light test
+    # imageFrame = Image.open("Valid_Traffic_Lights/traffic_light0_00072.jpg")
+
+    # # yellow light test
+    # imageFrame = Image.open("./Valid_Traffic_Lights/traffic_light2_00476.jpg")
+    
+    # # green light test
+    # imageFrame = Image.open("Valid_Traffic_Lights/traffic_light2_00012.jpg")
+
+    # convert image to numpy array
+    data = np.array(imageFrame)
+    # plt.imshow(imageFrame)
+    # plt.savefig('./light.png')
+    color = get_color(data)
     print(color)
